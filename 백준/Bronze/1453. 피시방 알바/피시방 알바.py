eaee2f1bd6afd7e63f40d6bdd9feffc0daa1d49e
EunBin <108652504+rhoeunbin@users.noreply.github.com>
2022-10-03 02:22:28 +0900

    [Bronze II] Title: 피시방 알바, Time: 108 ms, Memory: 113248 KB -BaekjoonHub

--- 백준/Bronze/1453. 피시방 알바/피시방 알바.py
+++ 백준/Bronze/1453. 피시방 알바/피시방 알바.py
@@ -1,13 +1,8 @@
+import sys
+
+input = sys.stdin.readline
+
 n = int(input())
-list_ = []
-num = input().split()
-cnt = 0  #거절당하는 사람 수
-
-for i in range(n):
-    
-    if num[i] not in list_:
-        list_.append(num[i])
-    else :
-        cnt += 1
-
-print(cnt)
+seat = list(map(int, input().split()))
+s = len(list(set(seat)))
+print(n-s)
