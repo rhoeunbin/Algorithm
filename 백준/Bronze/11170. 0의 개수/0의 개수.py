db17b6677683e20dca3490d0ed08332b69eba8ee
EunBin <108652504+rhoeunbin@users.noreply.github.com>
2022-10-05 20:59:29 +0900

    [Bronze I] Title: 0의 개수, Time: 288 ms, Memory: 114844 KB -BaekjoonHub

--- 백준/Bronze/11170. 0의 개수/0의 개수.py
+++ 백준/Bronze/11170. 0의 개수/0의 개수.py
@@ -1,13 +1,11 @@
 import sys
+
 input = sys.stdin.readline
 
-t = int(input())
+for i in range(int(input())):
+    n, m = map(int, input().split())
+    cnt = 0
 
-for _ in range(t):
-    a, b = map(int,input().split())
-    cnt= 0
-
-    for i in range(a, b+1): #a부터 b값 전체를 세기 때문에 
-        cnt += str(i).count('0') #숫자를 문자열로 바꾸어 count하면 됨
-    
+    for j in range(n, m + 1):
+        cnt += str(j).count("0")  # 숫자를 문자열로 바꾸어 count하면 됨
     print(cnt)
