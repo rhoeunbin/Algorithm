87183c91e2bd5db0f42fae17831bd5729aabb3a4
EunBin <108652504+rhoeunbin@users.noreply.github.com>
2022-10-20 00:23:40 +0900

    [Bronze I] Title: 가장 큰 금민수, Time: 120 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Bronze/1526. 가장 큰 금민수/가장 큰 금민수.py
+++ 백준/Bronze/1526. 가장 큰 금민수/가장 큰 금민수.py
@@ -1,10 +1,6 @@
 n = int(input())
 while True:
-    flag = True
-    for i in str(n):
-        if i!="4" and i!="7":
-            flag = False
-            n -= 1
-    if flag :
+    if len(str(n)) == str(n).count("4") + str(n).count("7"):
         print(n)
         break
+    n -= 1
