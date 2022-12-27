b5471593115b234363170cb5c0f467c6799ba5d5
none_null <108652504+rhoeunbin@users.noreply.github.com>
2022-12-27 19:46:04 +0900

    [Silver I] Title: 기타 레슨, Time: 144 ms, Memory: 128628 KB -BaekjoonHub

---
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.py
@@ -0,0 +1,28 @@
+n, m = map(int,input().split()) 
+# N : 레슨 수 , M : 블루레이 수
+num = list(map(int,input().split())) # 기타 레슨 데이터 저장 리스트
+start = 0
+end = 0
+
+for i in num:
+    if start < i:
+        start = i # 시작 인덱스 저장(num 리스트 중 최댓값)
+    end += i # 종료 인덱스 저장(num 리스트의 총합)
+
+while start <= end:
+    mid = (start + end) // 2 # 중간 인덱스
+    sum = 0 # 레슨 합
+    cnt = 0 # 현재 사용한 블루레이 개수
+    for i in range(n):
+        if sum + num[i] > mid:
+            cnt += 1
+            sum = 0 # 현재 블루레이에 저장할 수 없어 새로운 블루레이로 교체
+        sum += num[i]
+    if sum != 0: # sum이 0이 아니면 마지막 블루레이가 필요함 => cnt 올리기
+        cnt += 1
+    if cnt > m:
+        start = mid + 1
+    else:
+        end = mid - 1
+
+print(start)
