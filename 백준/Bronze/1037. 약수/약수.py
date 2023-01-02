54c45c80054072b40fcbd4dbabe56431b26f2a5d
none_null <108652504+rhoeunbin@users.noreply.github.com>
2023-01-02 18:21:08 +0900

    [Bronze I] Title: 약수, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.py
+++ 백준/Bronze/1037. 약수/약수.py
@@ -1,3 +1,4 @@
-a = int(input()) # n의 진짜 약수의 개수
-nums = list(map(int,input().split())) # n의 진짜 약수
-print(max(nums)*min(nums)) # nums리스트에서 작은 값 * 큰 값
+a = int(input()) #개수
+nums = list(map(int,input().split()))
+nums.sort()
+print(nums[0] * nums[-1]) 
