b8ac4702f35b39a3d737c48e129d646015d6a3fb
none_null <108652504+rhoeunbin@users.noreply.github.com>
2023-01-04 22:18:01 +0900

    [Silver III] Title: 소수 구하기, Time: 168 ms, Memory: 122828 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,10 +1,16 @@
-m, n = map(int,input().split())
+import math
+M, N = map(int,input().split())
+arr = [0]*(N+1)
 
-for num in range(m, n+1):
-    if num == 1:
+for i in range(2,N + 1):
+    arr[i] = i
+
+for i in range(2, int(math.sqrt(N) + 1)):
+    if arr[i] == 0:
         continue
-    for n in range(2, int(num** 0.5)+1):
-        if num % n ==0:
-            break
-    else:
-        print(num)
+    for j in range(i + i, N +1, i):
+        arr[j] = 0
+
+for i in range(M, N+1):
+    if arr[i] != 0:
+        print(arr[i])
