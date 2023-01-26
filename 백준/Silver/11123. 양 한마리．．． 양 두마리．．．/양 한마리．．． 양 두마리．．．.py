885b3d558f2440f6b97645af4435665974f70fb8
none_null <108652504+rhoeunbin@users.noreply.github.com>
2023-01-26 14:20:32 +0900

    [Silver II] Title: 양 한마리... 양 두마리..., Time: 252 ms, Memory: 119140 KB -BaekjoonHub

--- 백준/Silver/11123. 양 한마리．．． 양 두마리．．．/양 한마리．．． 양 두마리．．．.py
+++ 백준/Silver/11123. 양 한마리．．． 양 두마리．．．/양 한마리．．． 양 두마리．．．.py
@@ -1,7 +1,9 @@
 from collections import deque
 
 t = int(input())
-d = [(1, 0), (-1, 0), (0, -1), (0, 1)] # 이동 좌표값
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, 1, -1]
 
 def bfs(x, y):
     q = deque() # 새로 발견한 양의 위치 저장
@@ -9,11 +11,13 @@ def bfs(x, y):
     graph[x][y] = '.' # 중복되는 양의 수 방지
     while q:
         x, y = q.popleft()
-        for dy, dx in d: # 인접 양 찾기
-            nx, ny =  x + dx, y + dy
+        for d in range(4):
+            nx = x + dx[d]
+            ny = y + dy[d]
             if (0 <= nx < h) and (0 <= ny < w) and graph[nx][ny] == '#': # 그래프 안에 탐색하지 않은 양 있으먄
                 q.append((nx, ny)) # 새로 발견한 양은 큐에 넣음
                 graph[nx][ny] = '.' # 중복되는 양의 수 방지
+
 
 for _ in range(t):
     h, w = map(int, input().split())
