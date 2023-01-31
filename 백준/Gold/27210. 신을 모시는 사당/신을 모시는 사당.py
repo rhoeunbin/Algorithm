f7d12eea47c6727f6eb0065de6bfd98daa2d21f2
rhoeunbin <rhoeb21@gmail.com>
2023-01-31 16:58:48 +0900

    [Gold V] Title: 신을 모시는 사당, Time: 152 ms, Memory: 133556 KB -BaekjoonHub

--- 백준/Gold/27210. 신을 모시는 사당/신을 모시는 사당.py
+++ 백준/Gold/27210. 신을 모시는 사당/신을 모시는 사당.py
@@ -1,14 +1,12 @@
 n = int(input())
 direc = list(map(int, input().split()))
 
-cnt_l, cnt_r = [0] * n, [0] * n  # 왼쪽 1 리스트, 오른쪽 2 리스트
-
+ans = [0] * n
+cnt = 0
 for i in range(n):
-    if direc[i] == 1:  
-        cnt_l[i] = cnt_l[i - 1] + 1  
-        cnt_r[i] = max(0, cnt_r[i - 1] - 1)
-    else:  
-        cnt_r[i] = cnt_r[i - 1] + 1 
-        cnt_l[i] = max(0, cnt_l[i - 1] - 1)
-
-print(max(max(cnt_l), max(cnt_r)))  # 둘 중 최대값
+    if direc[i] == 1:
+        cnt += 1
+    else:
+        cnt -= 1
+    ans.append(cnt)
+print(max(ans) - min(ans))    
