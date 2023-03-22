86c3ce6db8c0148383f83571106ac501de5d5dad
none_null <108652504+rhoeunbin@users.noreply.github.com>
2023-03-22 21:58:57 +0900

    Update 벽 부수고 이동하기.py

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -15,7 +15,7 @@ def bfs():
     while q:
         x, y, w = q.popleft()
 
-        # 목표지점 도달 시 해당 위치까지의 거리 리턴
+        # 목표지점 도달 시 해당 위치까지의 거리 return
         if x == n - 1 and y == m - 1:
             return visited[x][y][w]
 
@@ -24,11 +24,11 @@ def bfs():
             ny = y + dy[i]
 
             if (0 <= nx < n) and (0 <= ny < m):
-                if not visited[nx][ny][w] and graph[nx][ny] == 0: # 방문하지 않고 육지라면
+                if not visited[nx][ny][w] and graph[nx][ny] == 0: # 현재 위치가 벽이 아니고, 방문X 
                     visited[nx][ny][w] = visited[x][y][w] + 1 # 다음 좌표로 넘어갈 때 +1
                     q.append((nx, ny, w)) 
 
-                # 현재 위치가 벽이고, 벽을 아직 부수지 않았다면
+                # 현재 위치가 벽이고 => 벽을 아직 부수지 않음
                 elif graph[nx][ny] == 1 and w == 0:
                     visited[nx][ny][w + 1] = visited[x][y][w] + 1
                     q.append((nx, ny, w + 1))
