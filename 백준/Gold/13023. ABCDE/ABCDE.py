121c99d487fd16175d4c07613e44a1584c60a038
none_null <108652504+rhoeunbin@users.noreply.github.com>
2023-04-12 22:17:39 +0900

    [Gold V] Title: ABCDE, Time: 728 ms, Memory: 116952 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.py
+++ 백준/Gold/13023. ABCDE/ABCDE.py
@@ -9,20 +9,19 @@ result = 0 # 정답 변수
 for i in range(m):
     a, b = map(int,input().split()) 
     graph[a].append(b)
-    graph[b].append(a)
+    graph[b].append(a) 
 
-def dfs(depth, x):# 깊이,현재 노드
+def dfs(friend, x):# 깊이,현재 노드
     global result
-    if depth == 4: # 친구 관계가 4번 이상 연결
+    if friend == 4: # 친구 관계가 4번 이상 연결
         result = True # 정답 표시 후 return
         return
-    visited[x] = True # 방문 표시
 
     for i in graph[x]: # 친구 관계 있는지 파악
         if not visited[i]: # 방문 안 했으면
-
-            dfs(depth + 1, i) # 재귀 수행 => 깊이 계속 수행
-    visited[x] = False # 방문 표시 해제
+            visited[x] = True # 방문 표시
+            dfs(friend + 1, i) # 재귀 수행 => 깊이 계속 수행
+            visited[x] = False # 방문 표시 해제
 
 
 for i in range(n):
