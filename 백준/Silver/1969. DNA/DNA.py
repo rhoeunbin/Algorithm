f5befefcfd02c940cbb8a35fde73b2f048ec3c31
none_null <108652504+rhoeunbin@users.noreply.github.com>
2023-06-11 16:44:47 +0900

    [Silver IV] Title: DNA, Time: 164 ms, Memory: 116444 KB -BaekjoonHub

---
+++ 백준/Silver/1969. DNA/DNA.py
@@ -0,0 +1,34 @@
+n, m = map(int, input().split())
+dna = []
+
+for i in range(n):
+    dna.append(list(map(str, input())))
+
+count = 0
+res = ''
+
+for i in range(m):
+    cnt = [0, 0, 0, 0]
+    for j in range(n):
+        if dna[j][i] == 'A':
+            cnt[0] += 1
+        elif dna[j][i] == 'C':
+            cnt[1] += 1
+        elif dna[j][i] == 'G':
+            cnt[2] += 1
+        else:
+            cnt[3] += 1
+    idx = cnt.index(max(cnt))
+
+    if idx == 0:
+        res += 'A'
+    elif idx == 1:
+        res += 'C'
+    elif idx == 2:
+        res += 'G'
+    else:
+        res += 'T'
+    count += n - max(cnt)
+
+print(res)
+print(count)
