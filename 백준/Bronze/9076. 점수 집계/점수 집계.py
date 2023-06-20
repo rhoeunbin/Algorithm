73383b4c256db33f9bcf0a57739abceea5d6baea
none_null <108652504+rhoeunbin@users.noreply.github.com>
2023-06-20 13:10:53 +0900

    [Bronze II] Title: 점수 집계, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/9076. 점수 집계/점수 집계.py
@@ -0,0 +1,10 @@
+t = int(input())
+
+for _ in range(t):
+    score = list(map(int, input().split()))
+    score.remove(max(score))
+    score.remove(min(score))
+    if max(score) - min(score) >= 4:
+        print('KIN')
+    else:
+        print(sum(score))
