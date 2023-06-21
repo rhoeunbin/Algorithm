2451c21dd0d577915826b1be00f96a9a99953797
none_null <108652504+rhoeunbin@users.noreply.github.com>
2023-06-21 20:09:21 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 520 ms, Memory: 124692 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
@@ -1,21 +1,20 @@
 import sys
 input = sys.stdin.readline
 
-def gold(a):
-    if a == 1:
-        return False
-    else:
-        for i in range(2, int(a**0.5)+1):
-            if a % i == 0: 
-                return False
-        return True
+num = [True] * 1000001
+
+for i in range(2, int(len(num)**0.5) + 1):
+    if num[i]:
+        for j in range(2 * i, 1000001, i):
+            num[j] = False
 
 while True:
     n = int(input())
     if n == 0:
         break
-    for i in range(3, n+1, 2):
-        if gold(i):
-            if gold(n-i):
-                print(n,'=', i,'+',n-i)
-                break
+    for i in range(3, n + 1, 2):
+        if num[i] and num[n - i]:
+            print(n,'=', i,'+',n-i)
+            break
+    else:
+        print('"Goldbach\'s conjecture is wrong."')
