1dc1b7af704657c2a191b4c8277925ef7b9b4284
none_null <108652504+rhoeunbin@users.noreply.github.com>
2023-10-23 13:48:13 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 460 ms, Memory: 169764 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,8 +1,18 @@
-s = input()
-for i in range(int(input())):
+import sys
+input = sys.stdin.readline
+
+s = input().rstrip()
+arr = [[0] * 26 for _ in range(len(s) + 1)]
+
+for i in range(1, len(s) + 1):
+    for j in range(26):
+        if ord(s[i - 1]) - 97 == j:
+            arr[i][j] = arr[i - 1][j] + 1
+        else:
+            arr[i][j] = arr[i - 1][j]
+
+q = int(input())
+for _ in range(q):
     a, l, r = input().split()
-    ans = 0
-    for j in range(int(l), int(r) + 1):
-        if s[j] == a:
-            ans += 1
-    print(ans)
+    a, l, r = ord(a) - 97, int(l), int(r)
+    print(arr[r + 1][a] - arr[l][a])
