fd2cc65cb1d2444e2b2701f1db8455fd1543df62
none_null <108652504+rhoeunbin@users.noreply.github.com>
2024-01-02 22:33:51 +0900

    [Bronze IV] Title: 단순한 문제 (Small), Time: 2872 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/25494. 단순한 문제 （Small）/단순한 문제 （Small）.py
@@ -0,0 +1,12 @@
+t = int(input())
+
+for i in range(t):
+    a, b, c = map(int, input().split())
+
+    cnt = 0
+    for x in range(1, a + 1):
+        for y in range(1, b + 1):
+            for z in range(1, c + 1):
+                if (x % y) == (y % z) and (x % y) == (z % x) and (y % z) == (z % x):
+                    cnt += 1
+    print(cnt)
