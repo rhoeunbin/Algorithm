c26918ebc770a8d2d8f93833e579ca2251dbf46d
none_null <108652504+rhoeunbin@users.noreply.github.com>
2024-01-11 16:43:29 +0900

    [Silver V] Title: 줄 세우기, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/11536. 줄 세우기/줄 세우기.py
@@ -0,0 +1,11 @@
+n = int(input())
+name = []
+for i in range(n):
+    name.append(input())
+
+if sorted(name) == name:
+    print('INCREASING')
+elif sorted(name, reverse=True) == name:
+    print('DECREASING')
+else:
+    print('NEITHER')
