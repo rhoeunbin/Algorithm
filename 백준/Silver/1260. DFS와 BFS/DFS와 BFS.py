7c90c2b4bdfdcc7718d92758290cc50ee26c85a0
none_null <108652504+rhoeunbin@users.noreply.github.com>
2024-02-29 18:33:46 +0900

    [Silver II] Title: DFS와 BFS, Time: 164 ms, Memory: 114324 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,42 +1,39 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
 
-n, m, start = map(int,input().split())
-# 노드 개수, 엣지 개수, 시작점
-arr = [[] for _ in range(n + 1)] # 인접 리스트에 그래프 데이터 저장
+n, m, start = map(int, input().split())
+arr = [[] for _ in range(n + 1)]
+visited = [False] * (n + 1)
 
 for _ in range(m):
-    a, b = map(int, input().split())
-    arr[a].append(b)
-    arr[b].append(a)
+    s, e = map(int,input().split())
+    arr[s].append(e)
+    arr[e].append(s)
 
 for i in range(n + 1):
     arr[i].sort()
-# 방문할 수 있는 노드가 여러 개일 경우, 번호가 작은 것을 먼저 방문하기 위해 정렬
 
 def dfs(v):
-    print(v, end=' ') # 현재 노드 출력
-    visited[v] = True # visiter 리스트에 현재 노드 방문 기록
-    for i in arr[v]: # 현재 노드의 연결 노드 중 방문하지 않은 노드로 dfs 실행(재귀함수형태)
+    print(v, end = ' ')
+    visited[v] = True
+    for i in arr[v]:
         if not visited[i]:
             dfs(i)
 
-visited =[False] * (n + 1) # visited 리스트 초기화
-dfs(start) # dfs 실행
-
 def bfs(v):
     q = deque()
-    q.append(v) # 큐 자료구조에서 시작 노드 삽입
+    q.append(v)
     visited[v] = True
-    while q: # 큐가 비어있을 때까지
-      now_node = q.popleft()
-      print(now_node, end = ' ') # 가져온 노드 출력
+    while q:
+        now = q.popleft()
+        print(now, end = ' ')
+        for i in arr[now]:
+            if not visited[i]:
+                visited[i] = True
+                q.append(i)
 
-      # 현재 노드의 연결 노드 중 미 방문 노드를 큐에 삽입하고 방문 리스트에 기록
-      for i in arr[now_node]:
-          if not visited[i]:
-              visited[i] = True
-              q.append(i)
-
+dfs(start)
 print()
 visited = [False] * (n + 1)
 bfs(start)
