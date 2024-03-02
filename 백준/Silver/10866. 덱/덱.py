5004573bffc64d75113f64f68a982faaa117a0b2
none_null <108652504+rhoeunbin@users.noreply.github.com>
2024-03-02 22:10:14 +0900

    [Silver IV] Title: 덱, Time: 68 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.py
+++ 백준/Silver/10866. 덱/덱.py
@@ -1,51 +1,43 @@
-from collections import deque 
+from collections import deque
 import sys
+input = sys.stdin.readline
 
-n=int(sys.stdin.readline())
-dq=deque()
+dq = deque()
+n = int(input())
+
 for _ in range(n):
-  order=list(sys.stdin.readline().split())
+    cmd = input().split()
 
-  if order[0]=='push_front':
-    dq.appendleft(order[1])
-
-  elif order[0]=='push_back':
-    dq.append(order[1])
-
-  elif order[0]=='pop_front':
-    if dq:
-      x=dq.popleft()
-      print(x)
-    else:
-      print(-1)
-
-  elif order[0]=='pop_back':
-    if dq:
-      x=dq.pop()
-      print(x)
-    else:
-      print(-1)
-
-  elif order[0]=='size':
-    print(len(dq))
-
-  elif order[0]=='empty':
-    if len(dq)==0:
-      print(1)
-    else:
-      print(0)
-
-  elif order[0]=='front':
-    if dq:
-      print(dq[0])
-    else:
-      print(-1)
-
-  elif order[0]=='back':
-    if dq:
-      print(dq[-1])
-    else:
-      print(-1)
-
-  else:
-    print('예외')
+    if cmd[0] == 'push_front':
+        dq.appendleft(cmd[1])
+    elif cmd[0] == 'push_back':
+        dq.append(cmd[1])
+    elif cmd[0] == 'pop_front':
+        if dq:
+            print(dq[0])    
+            dq.popleft()
+        else:
+            print(-1)
+    elif cmd[0] == 'pop_back':
+        if dq:
+            print(dq[len(dq) - 1])    
+            dq.pop()
+        else:
+            print(-1)
+    elif cmd[0] == 'size':
+        print(len(dq))
+    elif cmd[0] == 'empty':
+        if dq:
+            print(0)
+        else:
+            print(1)
+    elif cmd[0] == 'front':
+        if dq:
+            print(dq[0])
+        else:
+            print(-1)
+    elif cmd[0] == 'back':
+        if dq:
+            print(dq[len(dq) - 1])
+        else:
+            print(-1)
