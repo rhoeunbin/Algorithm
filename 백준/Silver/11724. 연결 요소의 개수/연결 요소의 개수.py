eacc4147a20c9da079560a9c6a65aea521868bfb
none_null <108652504+rhoeunbin@users.noreply.github.com>
2024-03-15 16:37:09 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 548 ms, Memory: 173404 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,32 +1,23 @@
-from collections import deque
-import sys
-input = sys.stdin.readline
-
-def bfs(v, graph, visited):
-    queue = deque()
-    queue.append(v)
-    visited[v] = True
-
-    while queue:
-        now = queue.popleft()
-
-        for j in graph[now]:
-            if not visited[j]:
-                queue.append(j)
-                visited[j] = True
-
 n, m = map(int, input().split())
-graph = [[] for _ in range(n+1)]
+
+arr = [[] for _ in range(n + 1)] # 인접 리스트
+visited = [False] * (n + 1) # 방문 리스트
+
+def dfs(v):
+    visited[v] = True # 방문 리스트에 현재 노드 방문 기록
+    for i in arr[v]:
+        if not visited[i]: # 방문하지 않은 노드로 dfs 실행
+            dfs(i)
+
 for _ in range(m):
-    u, v = map(int, input().split())
-    graph[u].append(v)
-    graph[v].append(u)
+    s, e = map(int, input().split()) # 인접 리스트에 그래프 저장
+    arr[s].append(e)
+    arr[e].append(s)
 
-result = 0
-visited = [False]*(n+1)
+cnt = 0
+for i in range(1, n + 1): # n의 개수만큼 반복
+    if not visited[i]: # 방문하지 않은 노드가 있다면
+        cnt += 1 # 연결 요소의 개수 +1
+        dfs(i)  # dfs 실행
 
-for i in range(1, n+1):
-    if not visited[i]:
-        result += 1
-        bfs(i, graph, visited)
-print(result)
+print(cnt)
