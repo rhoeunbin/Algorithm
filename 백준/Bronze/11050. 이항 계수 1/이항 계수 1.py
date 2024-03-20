9d044d8a4a3e8eccb4997b6891ef50606d277d6b
none_null <108652504+rhoeunbin@users.noreply.github.com>
2024-03-20 15:25:51 +0900

    [Bronze I] Title: 이항 계수 1, Time: 124 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -1,4 +1,13 @@
-from math import factorial
-n, k = map(int, input().split())
-b = factorial(n) // (factorial(k) * factorial(n - k))
-print(b)
+n, k = map(int,input().split())
+
+dp = [[0]*(n + 1) for _ in range(n + 1)]
+for i in range(0, n + 1):
+    dp[i][1] = i
+    dp[i][0] = 1
+    dp[i][i] = 1
+
+for i in range(2, n+ 1):
+    for j in range(1, i):
+        dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1]
+
+print(dp[n][k])
