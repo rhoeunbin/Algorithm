f4e0921f3227fb750cc5cfc818e014558d4836b1
none_null <108652504+rhoeunbin@users.noreply.github.com>
2024-03-28 16:34:32 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 144 ms, Memory: 113680 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -1,14 +1,12 @@
-import sys
-input = sys.stdin.readline
-N, K = map(int, input().split())
- 
-circle = [i for i in range(1, N+1)]
-delete = [] # 제거할 사람 리스트
-num = 0 # 제거할 사람 인덱스
+from collections import deque
 
-while circle:
-  num += K-1 # 0부터 시작하니깐
-  if num >= len(circle):
-    num %= len(circle) 
-  delete.append(str(circle.pop(num)))
-print(f"<{', '.join(map(str,delete))}>")
+n, k = map(int, input().split())
+arr = deque([i for i in range(1, n + 1)])
+res = []
+
+while len(arr) != 0:
+    for i in range(k - 1):
+        arr.append(arr.popleft())
+    res.append(str(arr.popleft()))
+
+print('<'+', '.join(res)+'>')
