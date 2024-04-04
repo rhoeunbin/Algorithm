5690849ab1a3f19e933869a1214f7404522d3bf9
none_null <108652504+rhoeunbin@users.noreply.github.com>
2024-04-04 21:48:21 +0900

    [Silver I] Title: 단지번호붙이기, Time: 140 ms, Memory: 113164 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,38 +1,37 @@
+import sys
+input = sys.stdin.readline
 from collections import deque
 
 n = int(input())
-counts = []
+graph = [list(map(int, input().strip())) for _ in range(n)]
+ans = []
 
-graph = [list(map(int, input())) for _ in range(n)] 
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
 
 def bfs(x, y):
-    dx = [-1, 1, 0, 0]  
-    dy = [0, 0, -1, 1]
-
-    q = deque()
-    q.append((x, y))
-
+    global cnt
     graph[x][y] = 0
-    cnt = 1
-
+    q = deque([(x, y)])
     while q:
         x, y = q.popleft()
-
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if (0 <= nx < n) and (0 <= ny < n) and graph[nx][ny] == 1:
+            if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == 1:
                 graph[nx][ny] = 0
-                q.append((nx, ny))
                 cnt += 1
-	
-    counts.append(cnt) 
+                q.append((nx, ny))
 
-for i in range(n):
-    for j in range(n):
-        if graph[i][j] == 1:
-            bfs(i, j)
+for x in range(n):
+    for y in range(n):
+        if graph[x][y] == 1:
+            cnt = 1
+            bfs(x, y)
+            ans.append(cnt)
 
-counts.sort()             
-print(len(counts), *counts, sep = '\n')
+ans.sort()
+print(len(ans))
+for i in range(len(ans)):
+    print(ans[i])
