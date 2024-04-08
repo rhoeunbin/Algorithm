fbee5da26ca35939720ad98f06b7a00ab9d416d7
none_null <108652504+rhoeunbin@users.noreply.github.com>
2024-04-08 16:29:17 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 400 ms, Memory: 134408 KB -BaekjoonHub

--- 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -1,28 +1,30 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
+
 n, m, r = map(int, input().split())
-arr = [[] for _ in range(n + 1)]
+graph = [[] for _ in range(n + 1)]
 visited = [0] * (n + 1)
 cnt = 1
-
 for _ in range(m):
     u, v = map(int, input().split())
-    arr[u].append(v)
-    arr[v].append(u)
-
+    graph[u].append(v)
+    graph[v].append(u)
 
 def bfs(s):
     global cnt
-    q = deque([s])
-    visited[s] = 1
+
+    q = deque([r])
+    visited[r] = 1
     while q:
-        now = q.popleft()
-        arr[now].sort()
-        for i in arr[now]:
-            if not visited[i]:
+        v = q.popleft()
+        graph[v].sort()
+        for g in graph[v]:
+            if visited[g] == 0:
                 cnt += 1
-                visited[i] = cnt
-                q.append(i)
-bfs(r)   
+                visited[g] = cnt
+                q.append(g)
+bfs(r)
 
 for v in visited[1:]:
     print(v)
