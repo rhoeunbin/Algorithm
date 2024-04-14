f10fd0a6fab83df0e06faecfeb990b089fd995ef
none_null <108652504+rhoeunbin@users.noreply.github.com>
2024-04-14 21:50:04 +0900

    [Silver III] Title: N과 M (1), Time: 164 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,7 +1,15 @@
-# 자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성
-from itertools import permutations #순열 라이브러리 사용
 n, m = map(int,input().split())
-arr = list(range(1, n+1)) # n개의 리스트 만들기
-answer = list(permutations(arr, m)) # arr에서 원소 개수가 m인 순열 뽑기
-for per in answer:
-    print(*per) # 튜플 형태를 없애기 위해 *사용
+arr = []
+
+def dfs():
+    if len(arr) == m:
+        print(' '.join(map(str, arr)))
+        return
+    
+    for i in range(1, n + 1):
+        if i not in arr:
+            arr.append(i)
+            dfs()
+            arr.pop()
+
+dfs()
