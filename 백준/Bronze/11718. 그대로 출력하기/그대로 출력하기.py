bead1b16499aebc6425911119c44bada3ffea402
none_null <108652504+rhoeunbin@users.noreply.github.com>
2024-04-30 13:42:49 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 124 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.py
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.py
@@ -1,7 +1,5 @@
-while True: # 예외 처리 구문 : try~ except~문
-    try: #(예외가 발생할 수도 있는 코드)
+while True :
+    try :
         print(input())
-    except EOFError: #except : (예외가 발생했을 경우 실행되는 코드)
-        break #EOFError : 입력이 끝남(End Of File) 에러
-      
-# 데이터가 없어 더 이상 값을 읽을 수 없을 때 발생하는 에러
+    except EOFError:
+        break
