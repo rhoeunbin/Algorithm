466cf95f96bbb73183faaaaacaf7414319de824f
none_null <108652504+rhoeunbin@users.noreply.github.com>
2024-05-07 15:14:27 +0900

    [Bronze V] Title: 검증수, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.py
+++ 백준/Bronze/2475. 검증수/검증수.py
@@ -1,3 +1,9 @@
-a, b, c, d, e = map(int,input().split())
+num = map(int, input().split())
 
-print((a**2+b**2+c**2+d**2+e**2)%10)
+res = 0
+
+for i in num:
+    res += i ** 2
+
+print(res % 10)  
+        
