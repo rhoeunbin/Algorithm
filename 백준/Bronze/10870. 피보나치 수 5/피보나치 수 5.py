7c16ab48b2aa9e268125cd8382e3577b97e6134a
none_null <108652504+rhoeunbin@users.noreply.github.com>
2024-05-20 15:02:59 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -1,7 +1,15 @@
 n = int(input())
 
-fibonacci = [0, 1]
-for i in range(2, n+1):
-    num = fibonacci[i-1] + fibonacci[i-2]
-    fibonacci.append(num)
-print(fibonacci[n])
+if n == 0:
+    print(0)
+elif n == 1:
+    print(1)
+else:
+    dp = [0] * (n + 1)
+    dp[0] = 0
+    dp[1] = 1
+    
+    for i in range(2, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+    
+    print(dp[n])
