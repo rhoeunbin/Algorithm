0e553cf3bfaf75bef0efac9679528c68d751148c
none_null <108652504+rhoeunbin@users.noreply.github.com>
2024-05-24 23:07:01 +0900

    [Silver V] Title: 수들의 합, Time: 112 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/1789. 수들의 합/수들의 합.py
+++ 백준/Silver/1789. 수들의 합/수들의 합.py
@@ -1,11 +1,10 @@
-s = int(input())
-cnt = 0
-total = 0
-
-while True:
-    cnt += 1
-    total += cnt
-    if total > s:
+n = int(input())
+s = 0
+ans = 0
+for i in range(1, n + 1):
+    s += i
+    ans += 1
+    if s > n:
+        ans -= 1
         break
-
-print(cnt - 1)
+print(ans)
