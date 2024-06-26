91dbf558ff8f76da8efaeeaec9631dcd607909f7
none_null <108652504+rhoeunbin@users.noreply.github.com>
2024-06-26 16:51:37 +0900

    [Bronze III] Title: ACM 호텔, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -1,10 +1,9 @@
-for i in range(int(input())):
-    H, W, N = map(int, input().split( )) # H = 각 호텔의 층 수, W = 각 층의 방 수, N= 몇 번째 손님
-
-    floor = N % H 
-    room_line = (N // H) + 1
-    if floor == 0: # 나머지가 0으로 나눠 떨어질때를 고려
-        floor = H
-        room_line -= 1
-    
-    print(floor * 100 + room_line)
+t = int(input())
+for _ in range(t):
+    h, w, n = map(int, input().split())
+    floor = n % h
+    room = (n // h) + 1
+    if floor == 0:
+        floor = h
+        room -= 1
+    print(floor * 100 + room)
