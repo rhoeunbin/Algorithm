3ee030f2d2800a09c4299a74fd6064cf870e8f93
none_null <108652504+rhoeunbin@users.noreply.github.com>
2024-06-28 15:56:07 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 128 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -1,13 +1,10 @@
 t = int(input())
-for i in range(t):
+for _ in range(t):
     k = int(input())
     n = int(input())
-    people = [i for i in range(1, n+1)]     # 0층
+    f = [x for x in range(1, n + 1)]
 
-    for x in range(k):
-        new = []
-        for y in range(n):
-            new.append(sum(people[:y+1]))   # 아래층의 1~n호 까지의 합
-        people = new.copy()
-        #print(people)		# peaple에 들어있는 값 출력해 봄
-    print(people[-1])       # K층 n호
+    for i in range(k):
+        for j in range(1, n):
+            f[j] += f[j - 1]
+    print(f[-1])
