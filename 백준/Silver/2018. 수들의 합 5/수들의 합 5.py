fd22d7790f6b725e7f54a45ac8131780885562c3
none_null <108652504+rhoeunbin@users.noreply.github.com>
2024-07-03 14:52:44 +0900

    [Silver V] Title: 수들의 합 5, Time: 188 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.py
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.py
@@ -1,10 +1,8 @@
 n = int(input())
-cnt = 1
-start, end = 1, 1
-hop = 1 # n = 15일 때 숫자 15만 뽑는 경우의 수를 미리 넣고 초기화 하기 때문
+cnt, start, end = 1, 1, 1
+hop= 1
 
-# 투 포인터 이동 원칙
-while end != n: # end가 n이 될 때까지 반복
+while end != n:
     if hop == n:
         cnt += 1
         end += 1
@@ -15,5 +13,4 @@ while end != n: # end가 n이 될 때까지 반복
     else:
         end += 1
         hop += end
-
 print(cnt)
