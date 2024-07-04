01ef776056bf82c1e6bcca5543ece7fb28f96432
none_null <108652504+rhoeunbin@users.noreply.github.com>
2024-07-04 15:22:32 +0900

    [Bronze II] Title: Hashing, Time: 88 ms, Memory: 108080 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,7 +1,7 @@
-L = int(input())
+l = int(input())
 string = input()
-answer = 0
+ans = 0
 
-for i in range(L):
-    answer += (ord(string[i])-96) * (31 ** i) #아스키 코드 값을 돌려주는 ord함수
-print(answer % 1234567891)
+for i in range(l):
+    ans += (ord(string[i])-96) * (31 ** i) #아스키 코드 값을 돌려주는 ord함수
+print(ans % 1234567891)
