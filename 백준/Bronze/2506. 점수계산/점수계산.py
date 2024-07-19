f06e56ad714dd22001578ba139b0778c7c374bb4
none_null <108652504+rhoeunbin@users.noreply.github.com>
2024-07-19 22:49:29 +0900

    [Bronze III] Title: 점수계산, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/2506. 점수계산/점수계산.py
+++ 백준/Bronze/2506. 점수계산/점수계산.py
@@ -1,12 +1,13 @@
 n = int(input())
-s = list(map(int, input().split()))
-cnt = 0
-ans = 0
+score = list(map(int, input().split()))
 
+ans = 0
+cnt = 0
 for i in range(n):
-    if s[i] == 1:
+    if score[i] == 1:
         cnt += 1
         ans += cnt
     else:
         cnt = 0
+
 print(ans)
