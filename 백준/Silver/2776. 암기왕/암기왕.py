8d69937ecc513c2b28d491e477591bdee76f2447
none_null <108652504+rhoeunbin@users.noreply.github.com>
2024-09-03 16:27:43 +0900

    [Silver IV] Title: 암기왕, Time: 896 ms, Memory: 323400 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.py
+++ 백준/Silver/2776. 암기왕/암기왕.py
@@ -2,27 +2,12 @@ t = int(input())
 
 for _ in range(t):
     n = int(input())
-    memo_one = list(map(int, input().split()))
-    memo_one.sort()
+    memo_one = set(map(int, input().split()))
     m = int(input())
     memo_two = list(map(int, input().split()))
 
-    for i in range(m):
-        find = False
-        t = memo_two[i] # 찾아야 하는 수
-        start = 0
-        end = n - 1
-        while start <= end:
-            midi = int((start + end) / 2) # 중간 인덱스
-            midv = memo_one[midi] # 중앙값
-            if midv > t: # 중앙값 > 타겟
-                end = midi - 1 # 끝에서 -1
-            elif midv < t:
-                start = midi + 1 # 앞에서 +1
-            else:
-                find = True # 찾음 => 1
-                break
-        if find:
+    for i in memo_two:
+        if i in memo_one:
             print(1)
         else:
             print(0)
