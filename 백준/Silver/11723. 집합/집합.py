fbd41fcdeb75193d7bbe5cf676d9184ff29ca9b1
none_null <108652504+rhoeunbin@users.noreply.github.com>
2024-09-25 22:34:39 +0900

    [Silver V] Title: 집합, Time: 1516 ms, Memory: 127764 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -4,31 +4,26 @@ input = sys.stdin.readline
 m = int(input())
 s = set()
 
-for i in range(m):
-    c = input().strip().split()
-    # print(s)    
-    
-    if len(c) == 1:
-        if c[0] == 'all':
-            s = set([i for i in range(1, 21)])
+for _ in range(m):
+    cal = list(input().split())
+    if cal[0] == 'add':
+        s.add(int(cal[1]))
+    elif cal[0] == 'check':
+        if int(cal[1]) in s:
+            print(1)
         else:
-            s = set()
-        continue
-    # print(s)
-
+            print(0)
+    elif cal[0] == 'remove':
+        try:
+            s.remove(int(cal[1]))
+        except:
+            pass
+    elif cal[0] == 'toggle':
+        if int(cal[1]) in s:
+            s.remove(int(cal[1]))
+        else:
+            s.add(int(cal[1]))
+    elif cal[0] == 'all':
+        s = set([i for i in range(1,21)])
     else:
-        com, tar = c[0], int(c[1])
-        if com == 'add':
-            s.add(tar)
-        elif com == 'remove':
-            s.discard(tar)
-        elif com == 'check':
-            if tar in s:
-                print(1)
-            else:
-                print(0)
-        elif com == 'toggle':
-            if tar in s:
-                s.discard(tar)
-            else:
-                s.add(tar)
+        s = set()
