82a140edb5a32a654e27401c75306b97fa7276e1
none_null <108652504+rhoeunbin@users.noreply.github.com>
2025-01-09 17:11:51 +0900

    [Silver I] Title: 숨바꼭질, Time: 128 ms, Memory: 112872 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -6,12 +6,14 @@ def bfs(v):
 
     while q:
         v = q.popleft()
-        if v == k: # 동생의 위치랑 같으면 종료
+        if v == k:
             return visited[v]
-        for i in (v-1, v+1, 2*v): # 이동할 수 있는 방향
+        for i in (v - 1, v + 1, 2*v):
             if 0 <= i <= 100000 and not visited[i]:
                 visited[i] = visited[v] + 1
                 q.append(i)
+
+
 
 n, k = map(int,input().split())
 visited = [0 for i in range(100001)]
