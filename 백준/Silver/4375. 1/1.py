d2a783c00b7d143c59ca30d16f0894e8d608eae3
none_null <108652504+rhoeunbin@users.noreply.github.com>
2025-03-13 16:07:43 +0900

    [Silver III] Title: 1, Time: 216 ms, Memory: 110576 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.py
+++ 백준/Silver/4375. 1/1.py
@@ -1,22 +1,17 @@
 while True:
     try:
         n = int(input())
+
     except:
         break
-    cnt = 1 # num의 자리수 체크
-    num = 0
+    
+    one = 1 # 1로만 이루어진 수
+    cnt = 1 # 자릿수
 
     while True:
-        num = num *10 + 1 # num에 나머지를 저장 모든 자리수가 1인 수 만듦
-        ''' num = 0
-            num = 0 * 10 +1 >> num은 1
-            num = 1 * 10 +1 >> num은 11
-            num = 11 * 10 +1 >> num은 111'''
-        num %= n
-        ''' 11 % 3 = (1 * 10 + 1) % 3 = 2
-            111 % 3 = (11 * 10 + 1) % 3 = 0
-        '''
-        if num == 0:
-            print(cnt)
+        if one % n != 0:
+            one = one * 10 + 1
+            cnt += 1
+        else:
             break
-        cnt += 1
+    print(cnt)
